e31bcaa9c06c949bfb63b217993c926259c72b03
Pikin <jadelrio1994@gmail.com>
2021-02-27 20:20:37 +0100

    haciendo el modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Input, Modal } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Post } from "./components/Post";
+import { db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      // cada vez q se agrege un post este codigo se dispara
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          posts: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  const handleLogin = (e) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* MODAL */}
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form>
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/640px-Instagram_logo.svg.png"
+                alt="Logo"
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="emali"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button onClick={handleLogin}>Login</Button>
+          </form>
+        </div>
+      </Modal>
+
+      {/* MODAL */}
+
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/640px-Instagram_logo.svg.png"
+          alt="Logo"
+        />
+
+        <Button onClick={() => setOpen(true)}>Sing up</Button>
+      </div>
+
+      <h1>Instagram clone app it's beging</h1>
+
+      {posts.map(({ id, posts }) => (
+        <Post
+          key={id}
+          username={posts.username}
+          caption={posts.caption}
+          imageUrl={posts.imageUrl}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB6nJ-1gorsXij-dQDLW4vtYUOKivveeAo",
+  authDomain: "instagram-clone-app-77dc9.firebaseapp.com",
+  projectId: "instagram-clone-app-77dc9",
+  storageBucket: "instagram-clone-app-77dc9.appspot.com",
+  messagingSenderId: "728653034180",
+  appId: "1:728653034180:web:023dc51e5170ab1248cf91",
+  measurementId: "G-S28X15YCT0",
+});
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.firestore();
+const storage = firebaseApp.firestore();
+
+export { db, auth, storage };
